8e8f93c0503a1b11f62ee8774ac5a5871703c757
fabianschmauder <fabianschmauder@gmail.com>
2020-05-28 15:31:21 +0200

    Init Uni search endpoint

--- src/main/java/de/neuefische/studentdbweb/controller/UniversityController.java
+++ src/main/java/de/neuefische/studentdbweb/controller/UniversityController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -35,4 +36,8 @@ public class UniversityController {
   }
 
 
+  @GetMapping("search")
+  public List<Student> searchStudents(){
+    return new ArrayList<>();
+  }
 }
