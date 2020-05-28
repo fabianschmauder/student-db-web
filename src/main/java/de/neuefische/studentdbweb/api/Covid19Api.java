66968534056a25455f50a1359ef776c026db2437
fabianschmauder <fabianschmauder@gmail.com>
2020-05-28 17:21:40 +0200

    Use current date

--- src/main/java/de/neuefische/studentdbweb/api/Covid19Api.java
+++ src/main/java/de/neuefische/studentdbweb/api/Covid19Api.java
@@ -5,21 +5,30 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 @Service
 public class Covid19Api {
 
 
-  public int numberOfActiveInfections(){
+  public int numberOfActiveInfections() {
+    LocalDate currentDate = LocalDate.now();
+
     RestTemplate restTemplate = new RestTemplate();
-    String url = "https://api.covid19api.com/total/country/germany?from=2020-05-24T00:00:00Z&to=2020-05-25T00:00:00Z";
+    String currentDayFormatted = currentDate.atStartOfDay().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+    String dayBeforeFormatted = currentDate.minusDays(1).atStartOfDay().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+
+
+    String url = "https://api.covid19api.com/total/country/germany?from=" + dayBeforeFormatted + "&to=" + currentDayFormatted;
 
     ResponseEntity<CovidApiCountryStats[]> responseEntity = restTemplate.getForEntity(url, CovidApiCountryStats[].class);
 
     CovidApiCountryStats[] covidData = responseEntity.getBody();
 
 
-    if(covidData.length > 0){
-      return covidData[0].getActive();
+    if (covidData.length > 0) {
+      return covidData[covidData.length - 1].getActive();
     }
     throw new IllegalStateException("No data form covid api");
   }
