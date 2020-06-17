3f287cc2d6effdb9ed411f640c57711a6890ec45
fabianschmauder <fabianschmauder@gmail.com>
2020-06-17 11:34:59 +0200

    Move to config

--- src/main/java/de/neuefische/studentdbweb/config/CorsConfig.java
+++ src/main/java/de/neuefische/studentdbweb/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package de.neuefische.studentdbweb;
+package de.neuefische.studentdbweb.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
